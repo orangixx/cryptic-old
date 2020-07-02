32ccfa6e261fc39cb28ab083d1ab8cfa64ba92e7
romanbeard112 <62947856+romanbeard112@users.noreply.github.com>
2020-07-01 21:24:08 -0500

    Updated The index.js file (Outdated)
    I updated the index.js file because it was 22 days old. (Because I've been adding new things to the index.js file aswell)

--- index.js
+++ index.js
@@ -1,11 +1,16 @@
 const Discord = require("discord.js");
-const client = new Discord.Client({ disableEveryone: true, disabledEvents: ["TYPING_START"] });
+const client = new Discord.Client({
+  disableEveryone: true,
+  disabledEvents: ["TYPING_START"]
+});
+const { MessageEmbed } = require("discord.js");
 const { readdirSync } = require("fs");
 const { join } = require("path");
-const { token, prefix } = require("./config.json")
+const { token, prefix } = require("./config.json");
 client.commands = new Discord.Collection();
-let xp = require("./xp.json");
+//let xp = require("./xp.json");
 require("dotenv").config();
+const guildInvites = new Map();
 
 const DBL = require("dblapi.js");
 const dbl = new DBL(process.env.DBL, client);
@@ -15,22 +20,62 @@ dbl.on("posted", () => {
 });
 
 dbl.on("error", e => {
-  console.log(`Oops! ${e}`);
+  //console.log(`Oops! ${e}`);
 });
 
 const fs = require("fs");
 
 require("./keepAlive.js")();
 
+client.on("inviteCreate", async invite =>
+  guildInvites.set(invite.guild.id, await invite.guild.fetchInvites())
+);
+
+client.on("ready", () => {
+  console.log(`${client.user.tag} is ready to kick some butt!`);
+  client.guilds.cache.forEach(guild => {
+    guild
+      .fetchInvites()
+      .then(invites => guildInvites.set(guild.id, invites))
+      //.catch(err => console.log(err));
+  });
+});
+
+  client.on("guildMemberAdd", async member => {
+  const cachedInvites = guildInvites.get(member.guild.id);
+  const newInvites = await member.guild.fetchInvites();
+  guildInvites.set(member.guild.id, newInvites);
+  try {
+    const usedInvite = newInvites.find(
+      inv => cachedInvites.get(inv.code).uses < inv.uses
+    );
+    const embed = new MessageEmbed()
+     .setTitle(`${member.user.tag}`)
+     .setDescription(
+        `Hello there ${member.user.username}!`)
+     .setFooter(`Member #${member.guild.memberCount}`)
+    const welcomeChannel = member.guild.channels.cache.find(
+      channel =>
+        channel.name === "│general");
+    if (welcomeChannel) {
+      welcomeChannel.send(embed).catch(err => console.log(err));
+    }
+  } catch (err) {
+   // console.log(err);
+  }
+});
+
 client.commands = new Discord.Collection();
-client.prefix = prefix
+client.prefix = prefix;
 client.queue = new Map();
 client.aliases = new Discord.Collection();
 
-const cmdFiles = readdirSync(join(__dirname, "commands")).filter(file => file.endsWith(".js"))
+const cmdFiles = readdirSync(join(__dirname, "commands")).filter(file =>
+  file.endsWith(".js")
+);
 for (const file of cmdFiles) {
-  const command = require(join(__dirname, "commands", file))
-  client.commands.set(command.name, command)
+  const command = require(join(__dirname, "commands", file));
+  client.commands.set(command.name, command);
 }
 
 fs.readdir("./commands/", (err, files) => {
@@ -56,17 +101,19 @@ fs.readdir("./events/", (err, files) => {
   });
 });
 
-client.on("warn", info => console.log(info));
+//client.on("warn", info => console.log(info));
 
-client.on("error", console.error)
+//client.on("error", console.error);
 
 client.on("message", message => {
   if (message.author.bot) return;
   if (!message.guild) return;
 
   if (message.content.startsWith(prefix)) {
-
-    const args = message.content.slice(prefix.length).trim().split(/ +/)
+    const args = message.content
+      .slice(prefix.length)
+      .trim()
+      .split(/ +/);
     const command = args.shift().toLowerCase();
 
     if (!client.commands.has(command)) {
@@ -74,18 +121,19 @@ client.on("message", message => {
     }
 
     try {
-      client.commands.get(command).execute(client, message, args)
+      client.commands.get(command).execute(client, message, args);
     } catch (err) {
-      console.log(err)
+      //   console.log(err)
       //message.reply("I am getting am error with the used command. please contact the bot owner/developers.")
     }
   }
 });
+
 client.on("message", async message => {
   if (message.author.bot) return;
   if (message.channel.type === "dm") return;
 
-  let xpAdd = Math.floor(Math.random() * 7) + 8;
+  /* let xpAdd = Math.floor(Math.random() * 7) + 8;
   console.log(xpAdd);
 
   if (!xp[message.author.id]) {
@@ -110,11 +158,22 @@ client.on("message", async message => {
   }
   fs.writeFile("./xp.json", JSON.stringify(xp), err => {
     if (err) console.log(err);
-  });
+  });*/
 
   let messageArray = message.content.split(" ");
   let cmd = messageArray[0];
   let args = messageArray.slice(1);
+
+  client.snipes = new Map();
+  client.on("messageDelete", function(message, channel) {
+    client.snipes.set(message.channel.id, {
+      content: message.content,
+      author: message.author.tag,
+      image: message.attachments.first()
+        ? message.attachments.first().proxyURL
+        : null
+    });
+  });
 });
 
 client.login(process.env.TOKEN);
