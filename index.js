7f0a66458d39d68c33e3786ae153a1e9c2daa61e
Queryzi <56193446+Queryzi@users.noreply.github.com>
2020-07-16 09:46:29 -0400

    Update index.js

--- index.js
+++ index.js
@@ -51,30 +51,6 @@ client.default_prefix = "c!";
 client.queue = new Map();
 client.aliases = new Discord.Collection();
 
-fs.readdir("./commands/", (err, files) => {
-  if (err) return console.log(err);
-  files.forEach(file => {
-    if (!file.endsWith(".js")) {
-    } else {
-      let props = require(`./commands/${file}`);
-      console.log("Successfully loaded " + file);
-      let commandName = file.split(".")[0];
-      client.commands.set(commandName, props);
-      if(props.aliases) client.aliases.forEach(a => client.aliases.set(a, props)) 
-    }
-  });
-});
-
-fs.readdir("./events/", (err, files) => {
-  if (err) console.log(err);
-  files.forEach(file => {
-    let eventFunc = require(`./events/${file}`);
-    console.log("Successfully loaded " + file);
-    let eventName = file.split(".")[0];
-    client.on(eventName, eventFunc.bind(null, client));
-  });
-});
-
 //client.on("warn", info => console.log(info));
 
 //client.on("error", console.error);
