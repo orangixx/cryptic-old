6191afbe732a0d5be8ec73d50efe4f6a9324c49c
romanbeard112 <62947856+romanbeard112@users.noreply.github.com>
2020-07-18 16:14:56 -0500

    Update messageDelete.js

--- events/messageDelete.js
+++ events/messageDelete.js
@@ -1,10 +1,11 @@
 const {MessageEmbed} = require("discord.js");
 module.exports = async(message) => {
   let embed = new MessageEmbed()
-  .setTitle(**Message Deleted**)
+  .setTitle("**Message Deleted**")
   .setDescription(`${message.author.tag} deleted a message in <#${message.channel.id}>`)
-  .addField(`Deleted Message:`, message.content, true)
-  let channel = message.guild.channels.cache.find(ch => ch.name === "")
+  .addField("Deleted Message:", message.content, true)
+  .setColor("RED")
+  let channel = message.guild.channels.cache.find(ch => ch.name === "server-log")
   if(!channel) return;
   channel.send(embed)
 }
