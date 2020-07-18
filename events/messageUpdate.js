ce6083124e925e5391cc409b834631254ba5ee84
romanbeard112 <62947856+romanbeard112@users.noreply.github.com>
2020-07-18 16:26:40 -0500

    Update messageUpdate.js

--- events/messageUpdate.js
+++ events/messageUpdate.js
@@ -1,1 +1,11 @@
-//
+const {MessageEmbed} = require("discord.js");
+module.exports = async(oldMessage, newMessage) => {
+  let embed = new MessageEmbed()
+  .setTitle("**Message Edited**")
+  .setDescription(`${oldMessage.author.tag} edited a message in <#${oldMessage.channel.id}`)
+  .addField("Old Message", oldMessage.content, true)
+  .addField("New Message", newMessage.content, true)
+  .setColor("GREEN")
+  let channel = oldMessage.guild.channels.cache.find(ch => ch.name === "server-log")
+  channel.send(embed)
+}
