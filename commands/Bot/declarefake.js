dc538e2d0037579895c0aafa83e8a6f232b40f9b
romanbeard112 <62947856+romanbeard112@users.noreply.github.com>
2020-08-04 16:47:30 -0500

    Update declarefake.js

--- commands/Bot/declarefake.js
+++ commands/Bot/declarefake.js
@@ -2,7 +2,7 @@ module.exports.run = async (bot, message, args) => {
   if (message.author.id !== "405497010385321995") return;
   message.channel.bulkDelete(5);
   message.delete();
-  message.channel.send(`@everyone **WARNING**: This server has been declared as fake by the developers of <@!612134650114080818>. The bot will be leaving this server shortly. If you think this is a mistake, let us know in our [Support Server](https://discord.gg/tpGHaGg)`);
+  message.channel.send(`@everyone **WARNING**: This server has been declared as fake by the developers of <@!612134650114080818>. The bot will be leaving this server shortly. If you think this is a mistake, let us know in our [Support Server](https://discord.gg/5wbQMxA)`);
   setTimeout(async () => {
     message.guild.leave();
   }, 10000);
