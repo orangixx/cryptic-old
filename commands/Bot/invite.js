b14fd1852a8c85a4a34703e833bf3372b2e2a967
romanbeard112 <62947856+romanbeard112@users.noreply.github.com>
2020-08-17 15:59:39 -0500

    Update invite.js

--- commands/Bot/invite.js
+++ commands/Bot/invite.js
@@ -6,9 +6,9 @@ module.exports.run = async (client, message, args) => {
       .setAuthor("Cryptic Invites", message.guild.iconURL)
       .addField(
         "Add the bot to your server",
-        `[Click here!](https://discordapp.com/api/oauth2/authorize?client_id=612134650114080818&permissions=8&scope=bot)`
+        `[Click here](https://discordapp.com/api/oauth2/authorize?client_id=612134650114080818&permissions=8&scope=bot)`
       )
-      .addField("Join our server", `[Click here!](https://discord.gg/5wbQMxA)`)
+      .addField("Join our server", `[Click here](https://discord.gg/NdHKxmV)`)
       .setColor("BLUE")
       .setTimestamp()
       .setThumbnail(boticon);
